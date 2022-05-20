756f6c9ad216ce07de7af345e22f98312ade2434
mistermopy <fgullihur@gmail.com>
2022-05-20 14:21:13 -0700

    Completed helloWorld, repeatString;

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(yourString, yourNum) {
+    let endResult = yourString;
+    if (yourNum == 1) {
+        return endResult;
+    } else if (yourNum == 0) {
+        return '';
+    } else if (yourNum < 0) {
+        return 'ERROR';
+    } else {
+        for (i = 0; i < (yourNum - 1); i++) {
+            endResult += yourString;
+        }
+        return endResult;
+    }
 };
 
 // Do not edit below this line
