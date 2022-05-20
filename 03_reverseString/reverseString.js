e0e0f47931680739e26e26af84d583aa238681f7
mistermopy <fgullihur@gmail.com>
2022-05-20 15:10:02 -0700

    Completed reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let endResult = '';
+    let strArray = string.split('');
+    for (i = string.length - 1; i >= 0; i--) {
+        endResult += strArray[i];
+    }
+    return endResult;
 };
 
 // Do not edit below this line
