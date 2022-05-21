277c6c2027cfc21b113c7e993a6616a8676d22de
mistermopy <fgullihur@gmail.com>
2022-05-20 19:18:51 -0700

    Completed sumAll;

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
 const sumAll = function() {
-
+    let result = 0;
+    if (arguments[0] > arguments[1]) {
+        for (let i = arguments[1]; i <= arguments[0]; i++) {
+            result += i;
+        }
+    } else if ((arguments[0] < 0) || (typeof arguments[0] !== typeof 0)) {
+        result = 'ERROR';
+    } else if ((arguments[1] < 0) || (typeof arguments[1] !== typeof 0)) {
+        result = 'ERROR';
+    } else {
+        for (let i = arguments[0]; i <= arguments[1]; i++) {
+            result += i;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
