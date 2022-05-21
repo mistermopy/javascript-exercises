d86abec76f22f4302703c604c1403369e16f4761
mistermopy <fgullihur@gmail.com>
2022-05-20 22:30:32 -0700

    Added solutions;

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,8 @@
 const reverseString = function(string) {
+    /*
+ return string.split('').reverse().join('');
+};
+*/
     let endResult = '';
     let strArray = string.split('');
     for (i = string.length - 1; i >= 0; i--) {
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
 const removeFromArray = function() {
     let myArray = Array.from(arguments[0]);
     for (let i = 0; i < arguments.length; i++) {
+        /*const removeFromArray = function (...args) {
+... const array = args[0];
+... const newArray = [];
+... array.forEach((item) => {
+..... if (!args.includes(item)) {
+....... newArray.push(item);
+....... }
+..... });
+... return newArray;
+... };*/
         for (let i = 0; i < myArray.length; i++) {
             if ((myArray[i] == arguments[1]) && (typeof myArray[i] == typeof arguments[1])) {
                 myArray.splice(i, 1);
