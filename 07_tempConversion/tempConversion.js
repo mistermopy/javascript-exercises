b6b5e7fd050e69ea821f4d70d995effff70dcd6a
mistermopy <fgullihur@gmail.com>
2022-05-20 21:24:37 -0700

    Completed tempConversion;

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(tempF) {
+  // [°C] = ([°F] − 32) × 5⁄9
+  let tempC = ((tempF - 32) * 5)/9;
+  return Math.round(tempC * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function(tempC) {
+ // [°F] = [°C] × 9⁄5 + 32
+  let tempF = (tempC * 9)/5 + 32;
+  return Math.round(tempF * 10) / 10;
 };
 
 // Do not edit below this line
